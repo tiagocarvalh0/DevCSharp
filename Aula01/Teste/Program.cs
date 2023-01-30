99e8c4ae19ca92a4ad8fcd550ae10e0de882e1d9
tiagocarvalh0 <tiagocrv501@gmail.com>
2023-01-30 11:34:33 -0300

    up

---
+++ Aula01/Teste/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Teste {
+
+    struct Product {
+
+        public Product(int id, string name, double price) {
+            Id = id;
+            Name = name;
+            Price = price;
+
+        }
+
+        public int Id;
+        public string Name;
+        public double Price;
+
+        public double PriceInDolar(double Dolar) {
+
+            return Price * Dolar;
+        } 
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Product mouse = new Product(1, "Mouse", 299.78);
+
+            Console.WriteLine(mouse.Id);
+            Console.WriteLine(mouse.Name);
+            Console.WriteLine(mouse.Price);
+        }
+    }
+}
---
+++ Aula02/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Aula02  {
+
+    class Program {
+
+        static void Main(string[] args) {
+            TelaMenu();
+        }
+        static void TelaMenu() {
+            while(true) {
+                Console.Clear();
+                Console.WriteLine("===== MENU =====");
+                Console.WriteLine("(1) = Soma.");
+                Console.WriteLine("(2) = Subtracao.");
+                Console.WriteLine("(3) = Multiplicacao.");
+                Console.WriteLine("(4) = Divisao.");
+                Console.WriteLine("(0) = SAIR.");
+                Console.Write("Selecione: ");
+
+                int OpcaoSelecionada = int.Parse(Console.ReadLine());
+                if(OpcaoSelecionada == 0)
+                    break;
+                if(OpcaoSelecionada == 1)
+                    Soma();
+                if(OpcaoSelecionada == 2)
+                    Subtracao();
+                if(OpcaoSelecionada == 3)
+                    Multiplicacao();
+                if(OpcaoSelecionada == 4)
+                    Divisao();
+            }
+        }
+
+        static void Soma() {
+            Console.Clear();
+            Console.Write("Primeiro Valor: ");
+            float PrimeiroValor = float.Parse(Console.ReadLine());
+            Console.Write("Segundo Valor: ");
+            float SegundoValor = float.Parse(Console.ReadLine());
+             float Resultado = PrimeiroValor + SegundoValor;
+            Console.Write("Soma = " + Resultado);
+        }
+
+        static void Subtracao() {
+            Console.Clear();
+            Console.Write("Primeiro Valor: ");
+            float PrimeiroValor = float.Parse(Console.ReadLine());
+            Console.Write("Segundo Valor: ");
+            float SegundoValor = float.Parse(Console.ReadLine());
+             float Resultado = PrimeiroValor - SegundoValor;
+            Console.Write("Subtracao = " + Resultado);
+        }
+
+        static void Divisao() {
+            Console.Clear();
+            Console.Write("Primeiro Valor: ");
+            float PrimeiroValor = float.Parse(Console.ReadLine());
+            Console.Write("Segundo Valor: ");
+            float SegundoValor = float.Parse(Console.ReadLine());
+            float Resultado = PrimeiroValor / SegundoValor;
+            Console.Write("Divisao = " + Resultado);
+        }
+
+        static void Multiplicacao() {
+            Console.Clear();
+            Console.Write("Primeiro Valor: ");
+            float PrimeiroValor = float.Parse(Console.ReadLine());
+            Console.Write("Segundo Valor: ");
+            float SegundoValor = float.Parse(Console.ReadLine());
+            float Resultado = PrimeiroValor * SegundoValor;
+            Console.Write("Multiplicacao = " + Resultado);
+        }
+    }
+}
+
